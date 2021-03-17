58eb3061054e4693cc01c59aaa33a856be57e232
luis <foreverluiscastro@gmail.com>
2021-03-17 00:58:33 -0400

    commit tree 634c4886c4b5e610a0b9a809235fe426a6dc2d50

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+if (ride < 400) {
+  return 'This one is on me!';
+  } else if (ride > 2500) {
+      return 'No can do.';
+  } else if (ride > 2000) {
+      return 'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destination){
   // Write your code here!
+  return (destination == 'NYC' ? 'Ok, sounds good.' : 'No go.');
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(goodbye){
   // Write your code here!
+  switch (goodbye) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    case 'thanks for everything':
+      return 'Bye.';
+  }
 }
